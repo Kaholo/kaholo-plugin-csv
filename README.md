18bbd3bb20b332f91e746cff9526e798de09d88a
dhrumil4u360@gmail.com <dhrumil4u360@gmail.com>
2022-01-17 19:35:29 +0530

    row insertion support added

--- README.md
+++ README.md
@@ -8,3 +8,11 @@ This method will create a CSV files
 1) File path: The path where to create the file + the file name
 2) Headers: Headers for the CSV
 3) Values: Values of the CSV
+
+## Method: Insert Rows into CSV
+This method will create a CSV files
+
+### Parameters:
+1) File path: The path where to create the file + the file name
+2) Headers: Headers for the CSV
+3) Values: Values to add to the CSV
--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 var fs = require('fs');
 var os = require('os');
 
-function _normalizeValue(value){
+function _normalizeValue(value) {
     return `"${value.replace(new RegExp('"', 'g'), '""')}"`
 }
 
@@ -9,29 +9,60 @@ function createCsv(action) {
     return new Promise((resolve, reject) => {
         if (!action.params.headers || !action.params.headers.length)
             return reject("No headers specified");
-        if (!action.params.data || !action.params.data.length)
-            return reject("No data specified");
 
         var output = [];
-        var headersRow = action.params.headers.map(header=>header.label);
+        var headersRow = action.params.headers.map(header => header.label);
         output.push(headersRow.join());
 
         action.params.data.forEach(dataRow => {
             let row = [];
-            action.params.headers.forEach(header=>{
+            action.params.headers.forEach(header => {
                 row.push(_normalizeValue(dataRow[header.field]));
             })
             output.push(row.join());
         });
 
-        fs.writeFile(action.params.filePath, output.join(os.EOL),function(err){
-            if(err) return reject(err);
-            
-            resolve({success : true, path : action.params.filePath});
+        fs.writeFile(action.params.filePath, output.join(os.EOL), function (err) {
+            if (err) return reject(err);
+
+            resolve({ success: true, path: action.params.filePath, headers: action.params.headers });
         });
     })
 }
 
+
+function insertRows(action) {
+    return new Promise((resolve, reject) => {
+        if (!action.params.headers || !action.params.headers.length)
+            return reject("No headers specified");
+        if (!action.params.data || !action.params.data.length)
+            return reject("No data specified");
+
+        var output = [];
+
+        action.params.data.forEach(dataRow => {
+            let row = [];
+            action.params.headers.forEach(header => {
+                row.push(_normalizeValue(dataRow[header.field]));
+            })
+            output.push(row.join());
+        });
+
+        let writer = fs.createWriteStream(action.params.filePath, {
+            flags: 'a'
+        });
+
+        writer.write(output.join(os.EOL), function (err) {
+            if (err) return reject(err);
+
+            resolve({ success: true, path: action.params.filePath });
+        });
+
+        writer.close();
+    })
+}
+
 module.exports = {
-    createCsv: createCsv
+    createCsv: createCsv,
+    insertRows
 };
