062b8f18f956f8f30264c0a2678fe39d52d68ed1
Dhrumil Bhankhar <dhrumil4u360@gmail.com>
2022-01-24 19:33:35 +0530

    row insertion support added (#5)
    * row insertion support added

--- src/app.js
+++ src/app.js
@@ -6,9 +6,7 @@ async function createCSV(action, settings) {
         throw "File path was not specified"
     if (!action.params.headers || !action.params.headers.length)
         throw "Headers were not specified";
-    if (!action.params.data || !action.params.data.length)
-        throw "Data was not specified";
-    
+
     let headers = parsers.array(action.params.headers);
     let rows = parsers.array(action.params.data);
     let filePath = parsers.string(action.params.filePath);
@@ -22,6 +20,29 @@ async function createCSV(action, settings) {
     return result;
 }
 
+
+async function insertRows(action) {
+    if (!action.params.filePath || !action.params.filePath.length)
+        throw "File path was not specified"
+    if (!action.params.headers || !action.params.headers.length)
+        throw "No headers specified";
+    if (!action.params.data || !action.params.data.length)
+        throw "No data specified";
+
+    let headers = parsers.array(action.params.headers);
+    let rows = parsers.array(action.params.data);
+    let filePath = parsers.string(action.params.filePath);
+
+    let result = await CSV.insertRows({
+        filePath,
+        headers,
+        rowsData: rows
+    })
+
+    return result;
+}
+
 module.exports = {
-    createCSV
+    createCSV,
+    insertRows
 };
--- src/csv-service.js
+++ src/csv-service.js
@@ -1,29 +1,39 @@
 const fs = require('fs').promises;
 const os = require('os');
-const { _normalizeValue } = require('./helpers');
+const { _buildOutput } = require('./helpers');
 
 module.exports = class CSV {
 
     static async createFile({ filePath, headers, rowsData }) {
-        let columnCount = headers.length;
         let output = [];
 
         output.push(headers.join());
 
-        // if one row passed, make it a nested array
-        if (!Array.isArray(rowsData[0])) rowsData = [rowsData]
-
-        rowsData.forEach(data => {
-            let row = []
-            for (let i = 0; i < columnCount; i++) {
-                row.push(_normalizeValue(data[i]));
-            }
-            output.push(row.join());
-        })
+        _buildOutput(output, rowsData, headers.length);
 
         try {
             await fs.writeFile(filePath, output.join(os.EOL));
-            return { success: true, path: filePath };
+            return { success: true, path: filePath, headers };
+        } catch (error) {
+            throw error
+        }
+    }
+
+    static async insertRows({ filePath, headers, rowsData }) {
+        let output = [];
+
+        _buildOutput(output, rowsData, headers.length);
+
+        try {
+            const writer = fs.createWriteStream(filePath, {
+                flags: 'a'
+            });
+
+            writer.write(output.join(os.EOL));
+
+            writer.close();
+
+            return { success: true, path: filePath, headers };
         } catch (error) {
             throw error
         }
